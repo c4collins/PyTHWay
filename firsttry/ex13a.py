57a04a24a12bf43e36d0831bcd75d225a84e0670
Connor Collins <connor.collins@gmail.com>
2012-04-10 09:31:44 -0400

    Moved previous files into the first-try folder, along with the first edition of the book.  Added the book's second edition to the main directory.

---
+++ firsttry/ex13a.py
@@ -0,0 +1,9 @@
+#ex13a.py Extra Credit #3 looked interesting
+
+from sys import argv
+
+script, divisor = argv
+
+max = raw_input("What number do you want to divide? ")
+
+print float(max) / int(divisor)
