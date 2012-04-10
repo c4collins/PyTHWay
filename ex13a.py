f176d4fb3256fe598d9e644701b217591339abba
Connor Collins <connor.collins@gmail.com>
2012-04-10 09:35:04 -0400

    removed files from the repo.

--- ex13a.py
+++
@@ -1,9 +0,0 @@
-#ex13a.py Extra Credit #3 looked interesting
-
-from sys import argv
-
-script, divisor = argv
-
-max = raw_input("What number do you want to divide? ")
-
-print float(max) / int(divisor)
