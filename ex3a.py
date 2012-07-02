29769205baa6d6581419185385432a43073428ce
Connor Collins <connor.collins+git@gmail.com>
2012-07-02 19:12:18 -0400

    manually renamed files

--- ex3a.py
+++
@@ -1,9 +0,0 @@
-width = 10 # centimetres
-height = 12 # centimetres
-length = 4 # centimetres
-
-area = 2 * (width * length + width * height + length * height)# cm^2
-volume = width * length * height # cm^3
-
-print "A cube that has a width of %d cm, a length of %d cm, and a height of %d cm, has a surface area of %d cm^2." % (width, length, height, area)
-print "A cube that has a width of %d cm, a length of %d cm, and a height of %d cm, has a volume of %d cm^3." % (width, length, height, volume)
