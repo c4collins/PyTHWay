2bf1be62e117b25f34eb0baedc49e0ec4071ce23
Connor Collins <connor.collins+git@gmail.com>
2012-07-04 06:53:57 -0400

    Added exercise 16.

--- ex16.py
+++ ex16.py
@@ -2,17 +2,25 @@ from sys import argv
 
 script, filename = argv
 
-print "We're going to erase %r." % filename
-print "If you don't want to do that, hit CTRL-C (^C)."
-print "If you do want that, hit Return, or Enter, or even '^M'"
+print "Opening %r for reading..." % filename
+target = open(filename, 'r') # opens the file with the read attribute
 
+print "It currently says:"
+print target.read()
+
+print "Closing the file."
+target.close() # close the file after reading
+
+print "We're going to erase %r." % filename
+print "If you don't want that, hit CTRL-C (^C)."
+print "If you do want that, hit Enter."
 raw_input("?")
 
-print "Opening the file..."
-target = open(filename, 'w')
+print "Opening %r..." % filename
+target = open(filename, 'w') # opens the file with the write attribute, which truncates the file
 
-print "Truncating the file.  Goodbye!"
-target.truncate()
+# print "Truncating the file.  Goodbye!"
+# target.truncate() # truncate deletes the entire contents of the file.
 
 print "Now I'm going to ask you for three lines."
 
@@ -22,12 +30,7 @@ line3 = raw_input("Line 3: ")
 
 print "I'm going to write these to the file."
 
-target.write(line1)
-target.write('\n')
-target.write(line2)
-target.write('\n')
-target.write(line3)
-target.write('\n')
+target.write("%s\n%s\n%s" % (line1, line2, line3)) # \n is the newline character
 
 print "And finally we close it."
 target.close()
