7fcb0b9872f7a8453ced10283fc87482572dc26c
c4collins <connor.collins+git@gmail.com>
2012-07-07 17:18:48 -0400

    Minor changes to exercise 35

--- ex35.py
+++ ex35.py
@@ -1,6 +1,7 @@
 from sys import exit
 
 def gold_room():
+    """This room is full of gold! How much do you want?"""
     print "This room is full of gold.  How much do you take?"
     
     next = raw_input('> ')
@@ -18,6 +19,7 @@ def gold_room():
         
         
 def bear_room():
+    """There's a bear here.  Try taunting the bear out of the way and then opening the door that's behind him.  Stay away from the cake."""
     print "There is a bear here."
     print "The bear has a bunch of honey."
     print "The fat bear is in front of another door."
@@ -32,7 +34,7 @@ def bear_room():
         elif next == "taunt bear" and not bear_moved:
             print "The bear moved from the door.  You can go through it now."
             bear_moved = True
-        elif next == "taunt bear" and bear_moved:
+        elif next == "taunt bear":
             dead("The bear gets pissed off and chews your leg off.")
         elif next == "open door" and bear_moved:
             gold_room()
@@ -40,6 +42,7 @@ def bear_room():
             print "I have no idea what that means."
             
 def cthulu_room():
+    """Sweet onions! Run away before he ruins you!"""
     print "Here you see the great evil Cthulu."
     print "He, it, whatever, stares at you and you go insane."
     print "Do you flee for your life or eat your head?"
@@ -54,11 +57,13 @@ def cthulu_room():
         cthulu_room()
 
 def dead(why):
+    """You have died..."""
     print "You have died!"
     print why, "Good job!"
     exit(0)
     
 def start():
+    """It was a dark and stormy night..."""
     print "You are in a dark room."
     print "There is a door to your right and left."
     print "Which one do you take?"
