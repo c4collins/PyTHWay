2e2c51e45b73b0c2cbe20f1f53645bb850ea9396
c4collins <connor.collins+git@gmail.com>
2012-07-07 16:13:14 -0400

    added a variable for the step factor to be used while generating the list as suggested in part three of the extra credit of exercise 33.

--- ex33a.py
+++ ex33a.py
@@ -1,15 +1,15 @@
-def build_numbers_list(top_num, bottom_num=0):
+def build_numbers_list(top_num, bottom_num=0,step=1):
     """
     Builds a list out of a given range of numbers (which is silly because you can assign it) and prints it out and returns the value.
-    Takes an argument of the top of the list, and an optional argument of the bottom of the list.
+    Takes an argument of the top of the list, an optional argument of the bottom of the list, and an optional argument of the step the is used when generating the list.
     """
     i = bottom_num
     numbers = []
     while i < top_num+1:
-        print "At the top, i is %d." % i
+        print "\nAt the top, i is %d." % i
         numbers.append(i)
         
-        i += 1
+        i += step
         print "Numbers now: ", numbers
         print "At the bottom, i is %d" % i
     
@@ -21,4 +21,4 @@ def build_numbers_list(top_num, bottom_num=0):
         
 build_numbers_list(6)
 build_numbers_list(34,32)
-build_numbers_list(4522,4500)
+build_numbers_list(4522,4500,6)
