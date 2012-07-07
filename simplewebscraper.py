f114ae3aa4f976cce4f9530e0313372dd56d8925
c4collins <connor.collins+git@gmail.com>
2012-07-07 10:02:17 -0400

    made some changes to the simplewebscaper.

--- simplewebscraper.py
+++ simplewebscraper.py
@@ -3,8 +3,15 @@
 from requests import * #gotta have my requests.
 # I imported as * as the script only uses requests 
 
-print "Welcome to the SimpleWebScraper!"
-print "What page would you like to retrieve?"
+print "\nWelcome to the SimpleWebScraper!"
+print "Answer the following questions and you'll be given the first match from the page."
+print """
+An an example, try entering 
+\t 'http://www.google.ca', 
+\t 'border-top', and 
+\t ';' 
+as the answers to the following questions.
+"""
 
 site = raw_input("Please enter the address of the page you wish to scrape: ")
 scrape_for = raw_input("What is the starting of the item you wish to search for: ")
@@ -19,5 +26,5 @@ print "\nRetrieving data from %s" % r.url
 # print r.json
 
 print "\nHere's what you're looking for:\n"
-print r.text[item_start:item_end+len(end_at)]
-print "\n"
+print "\t", r.text[item_start:item_end+len(end_at)]
+print
