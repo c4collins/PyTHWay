dc1a2667c0c9ecb099e330ca0d4761c2d187f8ac
c4collins <connor.collins+git@gmail.com>
2012-07-07 16:18:42 -0400

    rewrote exercise 33 to use for loops insted of while loops as suggested in part 5 of the extra credit, ex33b.py

---
+++ ex33b.py
@@ -0,0 +1,21 @@
+def build_numbers_list(top_num, bottom_num=0,step=1):
+    """
+    Builds a list out of a given range of numbers (which is silly because you can assign it) and prints it out and returns the value.
+    Takes an argument of the top of the list, an optional argument of the bottom of the list, and an optional argument of the step the is used when generating the list.
+    """
+    numbers = []
+    for i in range(bottom_num,top_num+1,step):
+        print "\nAt the top, i is %d." % i
+        numbers.append(i)
+        print "Numbers now: ", numbers
+        print "At the bottom, i is %d" % i
+    
+    print "The numbers: "
+
+    for num in numbers:
+        print num,
+    return numbers
+        
+build_numbers_list(6)
+build_numbers_list(34,32)
+build_numbers_list(4522,4500,6)
