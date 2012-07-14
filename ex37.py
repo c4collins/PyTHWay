a3bb3c1c9f3066fe9ee3b186c8224cc46e5dd4c0
Connor <connor.collins@gmail.com>
2012-07-14 09:37:17 -0400

    Completed the Keywords portion of exercise 37's definitions.

--- ex37.py
+++ ex37.py
@@ -1,4 +1,4 @@
-def print_definition_list(list):
+def print_keyword_list(list):
     for item in list:
         if len(item) <= 2:
             print "Keyword:\t %s\t\t%s" % (item)
@@ -7,10 +7,10 @@ def print_definition_list(list):
             print "Usage Example:"
             for ex_line in item[2]:
                 print "\t\t\t\t\t\t" + ex_line
-        print "-"*79
+        print "-"*79       
 
 
-definition_list = [
+keyword_list = [
     ('and',         "Boolean operator for AND"),
     ('del',         "Deletes an item from memory",          ("del foo[4]",)),
     ('from',        "Keyword used in import statements",    ("from sys import argv",)),
@@ -27,9 +27,21 @@ definition_list = [
     ('pass',        "Does nothing. Is useful to put in code blocks where something is required syntactically, but no actions need to be performed.  Is also useful as a placeholder while writing code allowing for structure without requiring content."),
     ('yield',       "Is used to return a generator statement (that is, an iterable object which can only be used once and is not stored in memory, so the list can be exceptionally long.  There is a great explanation on StackOverflow of what it is and how to use it."),
     ('break',       "Is used to end a loop.",       ("while True:","\tbreak",)),
-    ('except',      "Deals gracefully with error messages.  Instead of crashing the program immediately, \"except\" allows for catching the Exception, and returning an error message to the user.",        ("(i.e. while True:","\ttry:","\t\tx = int(raw_input(\"Please enter a number: \"))","\t\tbreak","\texcept ValueError:","\t\tprint \"Oops!  That was not a valid number.  Try again...\"",)),
+    ('except',      "Deals gracefully with error messages.  Instead of crashing the program immediately, \"except\" allows for catching the Exception, and returning an error message to the user.",        ("(i.e. while True:","\ttry:","\t\tx = int(raw_input(\"Please enter a number: \"))","\t\tbreak","\texcept (ValueError, TypeError):","\t\tprint \"Oops!  That was not a valid number.  Try again...\"",)),
     ('import',      "Used when importing other modules for use in a particular piece of code.",     ("from sys import argv as argument_variables",)) ,
-    ('print',       "Boolean operator"),
+    ('print',       "Prints whatever."),
+    ('class',      "Begins the definition of a class ",) ,
+    ('exec',      "Allows python code to be run from a string",        ("exec \"x=23\"",)) ,
+    ('in',      "Part of a for loop",        ("For line in file:","\tdo something",)) ,
+    ('raise',      "Can manually raise exceptions with custom error messages",        ("try","\traise NameError('HiThere')","except NameError:","\tprint \'an exception flew by!\'","\traise",)) ,
+    ('continue',      "Returns to the top of the loop and runs with the next iteration.  It's kind of like pass, except that it can only be used in loops, and it will not execute the remainder of the loop, while pass will.") ,
+    ('finally',      "Adds \'footer code\' to the bottom of a try block, so no matter what happens this code gets run at the end",        ("try:","\tf = file(\'poem.txt\')","\tprint f.readline()","finally:","\tf.close()",)) ,
+    ('is',      "Tests for IDENTITY, not equality of two objects.  Essentailly it says \"Do I have two names for the same object?\"",        ("a is b",)) ,
+    ('return',      "Specifies the statement to be returned by a function",        ("x = 15","return x",)) ,
+    ('def',      "Indicates the definition of a function",        ("def my_function(**args):",)) ,
+    ('for',      "Indicates the start of a for loop",        ("for line in file:","\tdo something",)) ,
+    ('lambda',      "A shortcut for creating anonymous, one-time, single-expression functions which return a value.  It's a kind of shorthand for the \'def\' statement",        ("sum = lambda x ,y : x + y",)) ,
+    ('try',      "Starts a try/except/finally statement",        ("try:","\tdo something","except TypeError:","\tprint \"Oops, something hs gone wrong\"","finally:","\tdo something else",)) ,
 ]
 
-print_definition_list(definition_list)
+print_keyword_list(keyword_list)
