6e6f05f2ae4dc731ff5b059f3c5d689e2cd47769
Connor Collins <connor.collins@gmail.com>
2012-08-05 10:16:02 -0400

    minor changes to ex37data.

--- ex37data.py
+++ ex37data.py
@@ -92,7 +92,7 @@ operator_list = [
     ('{}',       "Creates an empty hash/dict",       ("",)),
     ('@',       "Decorator expression",       ("",)),
     (',',       "list separator",       ("",)),
-    (':',       "",       ("",)),
+    (':',       "Indicates the start of an object definition.",       ("",)),
     ('.',       "Most often seen in dot notation of Object.methods",       ("",)),
     ('=',       "Equals (assigns object/value on right to object/variable on left)",       ("",)),
     (';',       "End of code line",       ("",)),
