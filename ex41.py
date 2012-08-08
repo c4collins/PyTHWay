cf37360ce9933140c22ae0b13562a0ffd117315d
Connor Collins <connor.collins@gmail.com>
2012-08-08 09:12:16 -0400

    updated the createItem and scanMode methods to work a little better.

--- ex41.py
+++ ex41.py
@@ -73,9 +73,9 @@ To complete the transaction and print the receipt, you can use the command "tota
 
     def scanMode(self):
         """Asks for input and directs the register how to deal with it."""
+       
         scanItem = raw_input("What is the item?  >> ")
-        
-        
+               
         if "total" in scanItem:         # break the loop and complete the transaction
             print "\n"
             return False            # returning False breaks the loop
@@ -84,75 +84,91 @@ To complete the transaction and print the receipt, you can use the command "tota
             self.voidItem()
             return True             # returning True continues the loop
             
-        elif  "createItem" in scanItem:          # add an intem to the inventory list
-            cr.createItem(raw_input("What item would you like to add?  >> "))
+        elif "createItem" in scanItem:          # add an intem to the inventory list
+            self.createItem()
             return True             # returning True continues the loop
             
         else:                    # Otherwise, assume the input is an item name
-            try:
-                scanQuantity = float(raw_input("How many are being purchased?  >> "))
-            except ValueError:
-                print "That is not a valid number, the quantity has defaulted to 1"
-                scanQuantity = 1
+            if scanItem not in self.items:      # checks if the item already has a price
+                print "That item does not appear to be in the item database.  If you wish to add it, please enter createItem at the scanning prompt."
+                return True
+            else:
+                try:
+                    scanQuantity = float(raw_input("How many are being purchased?  >> "))
+                except ValueError:
+                    print "That is not a valid number, the quantity has defaulted to 1"
+                    scanQuantity = 1
 
-            self.addItem(scanItem, scanQuantity)    # add the item to the purchase
-            return True                             # returning True continues the loop
+                self.addItem(scanItem, scanQuantity)    # add the item to the purchase
+                return True                             # returning True continues the loop
+
                 
 
 
     def addItem(self, item, quantity):
         """Add an item to the bill, and set the lastChargeAdded to that amount.  If the item being added is not in the items Dict, the cash register asks for the price and adds it."""
-        if item not in self.items:                              # checks if the item already has a price
-            print "That is not an item.  If you wish to add a new item, please enter createItem."
-        else:                                                   # if it does...
+        if item in self.items:
             self.total += self.items[item] * quantity           # increase the total sales price
             self.lastChargeAdded = self.items[item] * quantity  # set the lastChargeAdded to this transaction
             self.receiptItems.append((item, quantity))          # add this transaction to the receipt
+        else:
+            print "That item does not appear to be in the item database.  If you wish to add it, please enter createItem at the scanning prompt."
+    
 
 
-
-    def createItem(self, item):
+    def createItem(self):
         """Adds a new item to the list of available items"""
+        item = (raw_input("What item would you like to add?  >> "))
         newItem = [item,0,0,"FALSE",0,int(self.currentInventory[-1][5])+1]
-        while True:
+
+        for x in range(1,2+1):      # Range is non-inclusive
             try:                    # it asks for a price, 
-                newItem[1] = float(raw_input ("What is the regular price of 1 unit of %s?  >> " % item))
-                break
+                print "What is the",
+                if x == 1:
+                    print "regular",
+                else:
+                    print "sale",
+                newItem[x] = float(raw_input ("price of 1 unit of %s?  >> " % (item)))
             except ValueError:      # and will continue to ask until you give it one
                 print "Sorry, that's not a proper price.  The format should be like 4.69 "
 
-        while True:
-            try:
-                newItem[2] = float(raw_input ("What is the sale price of 1 unit of %s?  >> " % item))
-                break
-            except ValueError:
-                print "Sorry, that's not a proper price.  The format should be like 4.69 "
-        
+
         onSale = raw_input("Is %s currently on sale?  ( y / n )  >> " % item)[:1]
-        if (onSale == "y") or (onSale == "Y"):
+        if (onSale == "y") or (onSale == "Y"):  # Find out if the product is on sale
             newItem[3] = "TRUE"
-        else:
+        else:                                   # record the answer
             newItem[3] = "FALSE"
 
         while True:
-            try:
+            try:                    # Asks for the inventory quantity
                 newItem[4] = float(raw_input("How many %s are in stock?  >> " % item))
                 break
-            except ValueError:
+            except ValueError:      # until it works
                 print "That is not a valid quantity.  Pretty much any number will do"
         
-        if newItem[3] == "FALSE":
-             self.items[item] = newItem[1]
+        if newItem[3] == "FALSE":               # If the new item is not on sale
+             self.items[item] = newItem[1]      # Add the regular price to the inventory Dict
         else:
-             self.items[item] = newItem[2]
-        self.currentInventory.append(newItem)
-        self.addItemToInventory(newItem) 
-        print newItem
+             self.items[item] = newItem[2]      # otherwise add the regular price to the inventory Dict
+
+        self.currentInventory.append(newItem)   # Add new item to current Inventory
+        self.addItemToInventory(newItem)        # Write new item to inventory file
+        print "\n%.3f units of %s have been added to the inventory with a regular price of %s, a sale price of %s, and a sku of %d.  The item is currently %son sale.\n" % (newItem[4], newItem[0], self.currency(newItem[1]), self.currency(newItem[2]),newItem[5], self.onSale(newItem[3]))
+
+
+
+    def onSale(self, onSale):
+        """ Returns text value for item creation confirmation string. """
+        if onSale == "FALSE":
+            return "not "
+        else:
+            return ""
+
 
 
     
     def addItemToInventory(self, newItem):
-        """Adds a new item to the inventory data fil"""
+        """Adds a new item to the inventory data file"""
         with open(self.fileName, 'ab') as invFile:
             invFileWrite = csv.writer(invFile)
             invFileWrite.writerow(newItem)
