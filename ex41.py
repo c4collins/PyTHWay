e120e9dd56079a7abe804745d8392c621914e38d
Connor <connor.collins@gmail.com>
2012-08-12 20:11:05 -0400

    changed the header, item, and subtotal display so that they look nicer and convey information better.  I haven't quite finished formatting the totals, and haven't even started the footer yet.  There is a bunch of repeated code I'm going to replace once I've got he basics laid out, but I don't want to refactor until I can do it all at once.  And I updated CarhRegister.curreny to take a third parameter that defines when it can be more precise so things that are bash.00 like freight aren't made more precise for no reason.

--- ex41.py
+++ ex41.py
@@ -5,9 +5,10 @@ This file implements a CashRegister object, and when run boots a Cash Register o
 
 
 # Import statements
-import csv, glob, datetime
-date = datetime.datetime.date
+import csv, datetime
 
+# Global variables
+date = datetime.date.today()
 
 class CashRegister(object):
     """A CashRegister object tha can do everything a real cash register can"""
@@ -24,9 +25,11 @@ class CashRegister(object):
 
         # Declaring variables that will affect display
         self.currencySymbol =   "$"           # Localization
+        self.taxName        =   "HST/GST"     # Localization
+        self.taxRate        =   0.13        # Localization
         self.nameWidth      =   24            # Sets the length of the item name used on the receipt
         self.storeName      =   "Connor's Cash-Only Emporium"       # Sets the store name in the receipt
-        self.storeAddress   =   ["123 Fake St.","Waterloo, ON  N2V 2L4", "Canada"]
+        self.storeAddress   =   ["123 Fake St.","Waterloo, ON", "N2V 2L4 Canada"]
 
         # Declaring variables that are created from functions
         
@@ -44,7 +47,8 @@ From there, you can enter items and quantities as prompted.
 If no price exists in the system for the item you have entered, you will be asked to add one.
 Once a price is added it will exist in the system for that item until the cash register is restarted.
 
-To delete the last item added, you can use the command "void" at the "Which item?" prompt.
+To delete the last item added, you can use the command "void" at the "Which item?" prompt.  You can also specify voiding an item earlier than the last item by entering how far back the transaction is.  The second last item would be 2, the third last would be 3, etc.  You can also start from the beginning of the list, but with negative numbers: 0 is the first item scanned, -1 is the second item scanned, etc.
+
 To complete the transaction and print the receipt, you can use the command "total" at the "Which item?" prompt.
 """
     
@@ -75,13 +79,17 @@ To complete the transaction and print the receipt, you can use the command "tota
 
     def scanMode(self):
         """Asks for input and directs the register how to deal with it."""
-        scanItem = raw_input("What is the item?  >> ")
+        scanItem = raw_input("What is the item?  >> ").strip()
         if scanItem == "total":     # If the command 'total' is given, break the loop and complete the transaction
             print "\n"
             return False            # returning False breaks the loop
         elif scanItem == "void":    # If the command 'void' is given, undo the last transaction processed
             self.voidItem()
             return True             # returning True continues the loop
+        elif "void" in scanItem:
+            trash, num = scanItem.split(" ")
+            self.voidItem(-int(num))
+            return True 
         elif scanItem == "createItem":
             cr.createItem(raw_input("What item would you like to add?  >> "))
             return True
@@ -157,60 +165,111 @@ To complete the transaction and print the receipt, you can use the command "tota
 
 
 
-    def voidItem(self):
+    def voidItem(self, index = -1):
         """Voids the most recent transaction"""
         try:                                                                    # unless there is nothing in the list of items to be included on the receipt
-            void = self.receiptItems.pop()                                      # remove the last item on the list
+            void = self.receiptItems.pop(index)                                      # remove the last item on the list
             self.total -= self.items[void[0]] * void[1]                         # reduce the total by the appropriate amount
             print "%.3f of item: %s have been removed." % (void[1],void[0])     # print confirmation of the void
         except IndexError:  
-            print "There is nothing to void, the list of items scanned is empty."
+            print "There is nothing in the list at that postion."
+            try:
+                print "The most recent item in the list is: %s" % self.items[-1][0]
+            except IndexError:
+                print "There are no items currently accumulated on this register."
 
 
 
-    def currency(self,amount):
+    def currency(self,amount,precise=False):
         """takes a single number as an argument, returns a string of that number formatted as currency"""
-        return "%s%.2f" % (self.currencySymbol,float(amount))
-
-
-
-    def formatTotal(self):
-        """Displays the total with a currency indicator."""
-        return "The subtotal is %s" % (self.currency(self.total))
+        if ("%.2f" % float(amount) == "0.00") and (precise==True):
+            return "%s%.3f" % (self.currencySymbol,float(amount))
+        else:
+            return "%s%.2f" % (self.currencySymbol,float(amount))
+           
 
 
 
     def printReceipt(self):
         """Prints a header; the items purchased, their quantity, and the total for each item; a subtotal, discounts, taxes, the total, the method of payment, payment details (incl. change if applicable), and finally a footer"""
         receipt = []
-        for line in self.receiptHeader():
+
+        for line in self.printReceiptHeader():
             receipt.append(line.center(55 + self.nameWidth))
+        for line in self.printReceiptItems():
+            receipt.append(line)
+        for line in self.printReceiptTotal():
+            receipt.append(line)
 
-        for item in self.receiptItems:                              # for each item scanned
-            formattedItem = "%10s  of  %s at %12s  is  %12s" % ("%.3f" % (item[1]),item[0].ljust(self.nameWidth)[:self.nameWidth], self.currency(self.items[item[0]]), self.currency(float(item[1])*self.items[item[0]])) 
-            # create a string that contains -                   # the quantity      # The item name, to set width                   # The per-unit cost, and            # The total cost (quantity * per-unit cost)
-            receipt.append(formattedItem)                           # add the formattedItem string to the receipt
 
-        receipt.append(self.formatTotal().rjust(50 + self.nameWidth) + "\n")    # Display Subtotal
+#        receipt.append(self.formatTotal().rjust(50 + self.nameWidth) + "\n")    # Display Subtotal
         receipt.append("Thank you for shopping at:\t%s\n" % (self.storeName))   # This is a simple footer
 #        print self.receiptHeader()
         return receipt
 
     
 
-    def receiptHeader(self):
+    def printReceiptHeader(self):
         """Returns the receipt header text as a list of strings"""
         header = []
 
         header.append("Thank you for shopping at:")
-        header.append("\n")
-        header.append("%s" % self.storeName)
-        header.append("\n")
+        header.append("")
+        header.append("%s" % self.storeName.upper())
+        header.append("")
 
         for item in self.storeAddress:
             header.append("%s" % item)
 
+        header.append("")
+        header.append(date.strftime("%d-%h/%Y"))
+        header.append("")
+
         return header
+
+
+
+    def printReceiptItems(self):
+        """Returns each item in the receiptItems list as a formatted string for the receipt"""
+        items = []
+
+        items.append("="*(55 +self.nameWidth))
+        items.append("%10s      %s%18s%12s" % ("Quantity","Item".ljust(self.nameWidth),"Per Unit","Price"))  #  Header Row
+        items.append("="*(55 +self.nameWidth))
+
+        for item in self.receiptItems:                              # for each item scanned
+            formattedItem = "%10s  of  %s at %12s  is  %12s" % ("%.3f" % (item[1]),item[0].ljust(self.nameWidth)[:self.nameWidth], self.currency(self.items[item[0]],True), self.currency(float(item[1])*self.items[item[0]]))       # Create a string with the following information:
+            # the quantity
+            # The item name, to set width
+            # The per-unit cost, and
+            # The total cost (quantity * per-unit cost)
+            
+            items.append(formattedItem)                     # Add it to the list
+       
+        items.append("="*(55 +self.nameWidth))
+        items.append("")
+        
+        if len(self.receiptItems) == 0:
+            items[1] = "No items have been scanned".center(55+self.nameWidth)
+            del items[2]
+            
+
+        return items
+
+
+
+    def printReceiptTotal(self):
+        """Displays the total with a currency indicator."""
+        totalLines = []
+        
+        totalLines.append("The subtotal is %s".rjust(self.nameWidth) % (self.currency(self.total)))
+        totalLines.append("The freight cost is %s".rjust(self.nameWidth) % (self.currency(0)))
+        totalLines.append("The taxes (%s) is %s".rjust(self.nameWidth) % (self.taxName,self.currency(self.total*self.taxRate)))
+        totalLines.append("")
+        totalLines.append("="*(55+self.nameWidth))
+        totalLines.append("The total is %s".rjust(self.nameWidth) % (self.currency(self.total*(1+self.taxRate))))
+        
+        return totalLines
 
 
 
